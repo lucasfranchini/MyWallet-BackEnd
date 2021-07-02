fc1476319ade0f250bd2f94fb5399a3aacbf362b
Nivea <nivea@respondeai.com.br>
2021-07-02 14:31:08 -0300

    preparando o review

--- src/Validations/userSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const userSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required()
-})
-
-export default userSchema;
