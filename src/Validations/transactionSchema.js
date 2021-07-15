aa3eec5f6d95c958fedc504f0d0e28e037abf095
leleo <celso@respondeai.com.br>
2021-07-15 15:31:11 -0300

    code review

--- src/Validations/transactionSchema.js
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-const transactionSchema = Joi.object({
-    value: Joi.number().integer(),
-    description: Joi.string().required(),
-    type: Joi.string().valid('income','expense')
-})
-
-export default transactionSchema;
